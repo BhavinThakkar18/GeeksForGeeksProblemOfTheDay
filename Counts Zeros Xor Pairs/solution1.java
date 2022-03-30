41fd7018d18dc6e74b2ad2bdd6e47ba0b45f581d
Bhavin Majithia <36891191+BhavinThakkar18@users.noreply.github.com>
2022-03-30 19:55:46 +0530

    Create solution1.java

---
+++ Counts Zeros Xor Pairs/solution1.java
@@ -0,0 +1,86 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    
+		    Complete obj = new Complete();
+		    long res = obj.calculate(arr, sizeOfArray);
+		    System.out.println(res);
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static long calculate (int arr[], int n) {
+        int count=0;
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i=0;i<n;i++)
+        {
+            if(map.containsKey(arr[i]))
+            {
+                map.put(arr[i],map.get(arr[i])+1);
+            }
+            else
+            {
+                map.put(arr[i],1);
+            }
+        }
+        for (Map.Entry entry : map.entrySet()) {
+            int x=(int)entry.getValue();
+            count+=(x*(x-1)/2);
+        }
+        return count;
+    }
+    
+    
+}
+
+
